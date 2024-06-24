3c9722d18fbc3ad4662cb01f93acd2690eb8e9d5
Miguel Trabuco <migueltrabuco17@gmail.com>
2024-06-24 18:42:55 -0300

    update: refactor jwt service

--- src/services/JwtService.ts
+++ src/services/JwtService.ts
@@ -1,21 +1,10 @@
 import jwt from "jsonwebtoken";
 const SECRET: string = process.env.SECRET as string;
 
-// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
-export default class JwtService {
-	public static generateToken(id: string): string | null {
-		try {
-			return jwt.sign({ id }, SECRET);
-		} catch (e) {
-			return null;
-		}
-	}
+export const createToken = (payload: string) => {
+	return jwt.sign(payload, SECRET);
+};
 
-	public static decodeToken(token: string): jwt.JwtPayload | null {
-		try {
-			return jwt.decode(token) as jwt.JwtPayload;
-		} catch (e) {
-			return null;
-		}
-	}
-}
+export const verifyToken = (token: string) => {
+	return jwt.verify(token, SECRET);
+};
