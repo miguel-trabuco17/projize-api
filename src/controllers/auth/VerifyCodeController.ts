1cf773cdd46cd1e7e9935d8ee211bfb6fb85bdec
Miguel Trabuco <migueltrabuco17@gmail.com>
2024-06-26 21:41:39 -0300

    feat: add verify code route and controller

---
+++ src/controllers/auth/VerifyCodeController.ts
@@ -0,0 +1,28 @@
+import VerificationCodeModel from "../../models/VerificationCodeModel";
+import type VerificationCodeInterface from "../../interfaces/VerificationCodeInterface";
+import type { Request, Response } from "express";
+
+export default class VerifyCodeController {
+	public async handle(req: Request, res: Response): Promise<Response> {
+		const { code }: { code: string } = req.body;
+
+		if (!code) {
+			return res.status(400).json({ message: "Code is required" });
+		}
+
+		const verificationCodeDocument: VerificationCodeInterface | null =
+			await VerificationCodeModel.findOne({ code });
+
+		if (!verificationCodeDocument) {
+			return res.status(400).json({ message: "Invalid code" });
+		}
+
+		if (verificationCodeDocument.verified) {
+			return res.status(200).json({ message: "Verified" });
+		}
+
+		await VerificationCodeModel.updateOne({ code }, { verified: true });
+
+		return res.status(200).json({ message: "Verified" });
+	}
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,11 +3,14 @@ const router = express.Router();
 
 // Importing controllers
 import SendCodeController from "../controllers/auth/SendCodeController";
+import VerifyCodeController from "../controllers/auth/VerifyCodeController";
 
 // Controllers
 const sendCodeController = new SendCodeController();
+const verifyCodeController = new VerifyCodeController();
 
 // Routes
 router.post("/send-code", sendCodeController.handle);
+router.post("/verify-code", verifyCodeController.handle);
 
 export default router;
