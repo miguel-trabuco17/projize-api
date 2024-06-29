45ba8bf6f2b82b2bba60d995393166d352651eb6
Miguel Trabuco <migueltrabuco17@gmail.com>
2024-06-29 16:01:19 -0300

    Add nodemailer service

--- src/services/NodemailerService.ts
+++ src/services/NodemailerService.ts
@@ -8,21 +8,25 @@ export default class NodemailerService {
 		secure: false,
 		auth: {
 			user: process.env.MAILGUN_USER as string,
-			pass: process.env.MAILGUN_PASS as string,
+			pass: process.env.MAILGUN_PASSWORD as string,
 		},
 	});
 
-	public async sendEmail(body: string, subject: string, email: string) {
+	public async sendEmail({
+		body,
+		subject,
+		email,
+	}: { body: string; subject: string; email: string }) {
 		try {
 			await this.transporter.sendMail({
-				from: "Projize <support@projize.me>",
+				from: '"Projize" <noreply@projize.me>',
 				to: email,
 				subject: subject,
 				html: body,
 			});
-
 			return true;
 		} catch (error) {
+			console.log(error);
 			return false;
 		}
 	}
